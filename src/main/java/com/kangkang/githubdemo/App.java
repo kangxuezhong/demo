7a0579587b982307bfa96008b62778e4cd8be0b5
kangxuezhong <kxz0316@163.com>
2020-02-11 15:07:20 +0800

    first

---
+++ src/main/java/com/kangkang/githubdemo/App.java
@@ -0,0 +1,13 @@
+package com.kangkang.githubdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
